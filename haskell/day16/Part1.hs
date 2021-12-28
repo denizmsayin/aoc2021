80363355a175a468e404a5b7fc0016508f1a8f2a
DenizMS <denizmsayin@gmail.com>
2021-12-28 18:21:48 +0300

    updated haskell d16p1 with better state

--- haskell/day16/Part1.hs
+++ haskell/day16/Part1.hs
@@ -17,48 +17,47 @@ hex2bits = n2b 4 . hex2num
 b2n :: [Bool] -> Int
 b2n = foldl (\a x -> fromEnum x + 2*a) 0
 
-parsePacket :: State [Bool] (Int, Int)
+parsePacket :: State [Bool] Int
 parsePacket = do
     ver_num <- b2n <$> state (splitAt 3)
     type_id <- b2n <$> state (splitAt 3)
     if type_id == 4
        then do
            val_len <- consumeValuePacket
-           pure (ver_num, 6 + val_len)
+           pure ver_num
        else do
-           (sub_ver_nums, sub_len) <- parseSubpackets
-           pure (ver_num + sum sub_ver_nums, 7 + sub_len)
+           sub_ver_nums <- parseSubpackets
+           pure $ ver_num + sum sub_ver_nums
   where
-    consumeValuePacket :: State [Bool] Int
+    consumeValuePacket :: State [Bool] ()
     consumeValuePacket = do
         slice <- state $ splitAt 5
         if head slice -- continuation bit
-           then (+5) <$> consumeValuePacket
-           else pure 5
+           then consumeValuePacket
+           else pure () 
     combineParse :: ([Int], Int) -> (Int, Int) -> ([Int], Int)
     combineParse (xs, c) (x, xc) = (x : xs, c + xc)
-    parseSubpackets :: State [Bool] ([Int], Int)
+    parseSubpackets :: State [Bool] [Int]
     parseSubpackets = do
         len_id <- head <$> state (splitAt 1)
-        (parses, len) <- if len_id
+        if len_id
            then do
                num_subpackets <- b2n <$> state (splitAt 11)
-               parses <- replicateM num_subpackets parsePacket
-               pure (parses, 11)
+               replicateM num_subpackets parsePacket
            else do
                num_bits <- b2n <$> state (splitAt 15)
-               parses <- parseNBits num_bits
-               pure (parses, 15)
-        pure $ foldl combineParse ([], len) parses
-    parseNBits 0 = pure []
-    parseNBits n = do
-        parse@(_, len) <- parsePacket
-        rest <- parseNBits (n - len)
-        pure $ parse : rest
+               subpacket_bits <- state (splitAt num_bits)
+               pure (evalState parseUntilNone subpacket_bits)
+    parseUntilNone :: State [Bool] [Int]
+    parseUntilNone = do
+        s <- get
+        if null s
+           then pure []
+           else liftM2 (:) parsePacket parseUntilNone
 
 main :: IO ()
 main = do
     bits <- concatMap hex2bits . head . lines <$> getContents
-    let (ver_sum, _) = evalState parsePacket bits
+    let ver_sum = evalState parsePacket bits
     print ver_sum
 
