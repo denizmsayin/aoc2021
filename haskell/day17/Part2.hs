41043b613b095370d18fa1273fbf0837b1c13578
DenizMS <denizmsayin@gmail.com>
2021-12-29 15:49:34 +0300

    haskell d17p2 slightly off for big inputs :(

--- haskell/day17/Part2.hs
+++ haskell/day17/Part2.hs
@@ -1,11 +1,18 @@
 import qualified Data.List as L
 import Data.List.Split (splitOn)
 
+import Text.Printf
+import Debug.Trace
+
 -- Usually, (/) produces fractionals. But we only use Ints, and
 -- writing `quot` every time is a pain. Let's replace the default!
 import Prelude hiding ((/))
 (/) :: Int -> Int -> Int
 (/) = quot
+
+-- Fancy ceil div
+(/^) :: Int -> Int -> Int
+(/^) x y = (x + y - 1) / y
 
 -- Same with mod to make similar
 (%) :: Int -> Int -> Int
@@ -27,7 +34,7 @@ primeFactorize a = go a primes 0
         go _ _ _ = error "Should be unreachable"
 
 divisors :: Int -> [Int]
-divisors a = go $ primeFactorize $ abs a
+divisors x = go $ primeFactorize $ abs x
   where go [] = [1]
         go ((x, n):factors) = 
             let rest = go factors
@@ -36,9 +43,9 @@ divisors a = go $ primeFactorize $ abs a
 
 divsBetween :: Int -> Int -> Int -> Int
 divsBetween d a b = 
-    let a' = a + ((d - a % d) % d)
-        b' = b - b % d
-     in relu $ (b' - a') / d + 1
+    let s = a /^ d -- ceil div.
+        e = b / d
+     in relu $ e - s + 1
 
 generateVyN :: Int -> [(Int, Int)]
 generateVyN yTarget = 
@@ -70,20 +77,47 @@ triangularsBetween a' b' =
         end = if bt > b then br - 1 else br
      in relu $ end - start + 1
 
-hasPossibleVxs :: (Int, Int) -> Int -> Bool
-hasPossibleVxs (xmin, xmax) n = 
+minmax :: [Int] -> (Int, Int)
+minmax [] = error "minmax: empty list"
+minmax (x:xs) = L.foldl' (\(l, u) x -> (min l x, max u x)) (x, x) xs
+
+-- It is possible to count - unique - n's directly, given a set of
+-- ns. But it's probably a huge pain...
+-- For fp:
+-- xmin + max_sum <= lcm * vx <= xmax + min_sum
+-- Will be the expression for intersections. However, for m amount
+-- of ns, need to calculate (m^2) intersections. Bad! A faster
+-- method is necessary. Think about the expression...
+-- For sp:
+-- Can simply take triangulars in [sx, n_max_sum]
+--
+-- For a certain vy, there's an acceptable range [nmin, nmax]. So
+-- it's not an arbitrary set, which makes this easier.
+-- I can determine the minimum (xmin + nsum / n) and the maximum
+-- as bounds, which means I can find fp in O(1). sp was already O(1).
+-- Excellent!
+numPossibleVxs :: (Int, Int) -> (Int, Int) -> Int
+numPossibleVxs (xmin, xmax) (nmin, nmax) = 
     -- First part, those between:
     -- xmin + n*(n-1)/2 <= n*vx <= xmax + n*(n-1)/2
     -- Valid for vx >= n only, so min will be max(xmin + sum, n*n)
+    -- Got to determine the loosest bounds in the given range. 
+    -- I think I could use some math tricks with the derivative here, 
+    -- but I want to take it easy and just filter the min/max from the list
+    -- [nmin..nmax]. It will be fast enough since we go over each N while
+    -- generating Vy's anyway (although that could also be optimised to stop
+    -- early).
+    let lowerBound = minimum $ map (\n -> max ((xmin + triang (n - 1)) /^ n) n) [nmin..nmax]
+        upperBound = maximum $ map (\n -> (xmax + triang (n - 1)) / n) [nmin..nmax]
+        fp = relu $ upperBound - lowerBound + 1
     -- Second part, triangulars between
     -- sx <= vx * (vx + 1) / 2 <= ex 
-    -- Valid for vx < n. Thus, max will min(ex, n * (n + 1) / 2 - 1)
-    let nsum = triang (n - 1) 
-        fpMin = max (xmin + nsum) (n * n)
-        fp = divsBetween n fpMin (xmax + nsum)
-     in (fp > 0) || (let spMax = min xmax (triang n - 1)
-                         sp = triangularsBetween xmin spMax
-                       in sp > 0)
+    -- Valid for vx < n. Thus, max will be min(ex, n * (n + 1) / 2 - 1)
+    -- To keep this lose, we should take nmax. But, we've already found
+    -- all vx up to lowerBound, so that also should be taken into account.
+        spMax = min (triang (lowerBound - 1)) $ min xmax (triang nmax - 1)
+        sp = triangularsBetween xmin spMax
+     in fp + sp
 
 parseArea :: String -> ((Int, Int), (Int, Int))
 parseArea line = let Just rest = L.stripPrefix "target area: " line
@@ -95,8 +129,8 @@ main :: IO ()
 main = do
     (xrange, (ymin, ymax))  <- parseArea . head . lines <$> getContents
     let vyns = concatMap generateVyN [ymin..ymax]
-        viablePairs = filter (hasPossibleVxs xrange . snd) vyns
-        maxVy = maximum $ map fst viablePairs
-        maxHeight = triang maxVy 
-    print maxHeight
+        grouped = L.groupBy (\a b -> fst a == fst b) $ L.sortOn fst vyns
+        ranged = map (\g -> (fst (head g), minmax (map snd g))) grouped
+        counts = map (numPossibleVxs xrange . snd) ranged
+    print $ sum counts
 
