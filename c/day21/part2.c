07d78c9cfdf0f245e1a30624a1594c8a53d38a7c
DenizMS <denizmsayin@gmail.com>
2022-01-12 12:34:28 +0300

    small array dim switcharound for d21

--- c/day21/part2.c
+++ c/day21/part2.c
@@ -21,7 +21,7 @@ static inline lli max(lli a, lli b) { return a > b ? a : b; }
 
 int main(void)
 {
-    static struct pair table[10][10][21][21][2];
+    static struct pair table[21][21][10][10][2];
     int pos[2];
     struct pair *initial;
 
@@ -30,6 +30,8 @@ int main(void)
 
     // Try to fill the DP table bottom-up this time,
     // instead of doing memoization like in Rust & Haskell
+    // Kind of wasteful since I compute every state, but
+    // I wanted to do something slightly different this time.
     static const lli MULTS[] = { 1, 3, 6, 7, 6, 3, 1 };
     static const int ROLLS[] = { 3, 4, 5, 6, 7, 8, 9 };
     static const size_t NROLLS = sizeof(ROLLS) / sizeof(ROLLS[0]);
@@ -51,7 +53,7 @@ int main(void)
                         } else if (s[1] >= 21) {
                             value = (struct pair) {{ 0, 1 }};
                         } else {
-                            value = table[p[0]][p[1]][s[0]][s[1]][i];
+                            value = table[s[0]][s[1]][p[0]][p[1]][i];
                         }
 
                         // Combinations of three 1-2-3 dice rolls are fun:
@@ -63,7 +65,7 @@ int main(void)
                             s[j] -= p[j] + 1;
                             p[j] = (p[j] + (10 - roll)) % 10; // 10 - roll to avoid neg.
                             if (s[j] >= 0 && s[0] < 21 && s[1] < 21) {
-                                struct pair *pair = &table[p[0]][p[1]][s[0]][s[1]][j];
+                                struct pair *pair = &table[s[0]][s[1]][p[0]][p[1]][j];
                                 *pair = tupadd(*pair, tupscmul(MULTS[k], value));
                             }
                             s[j] = prev_score;
@@ -75,7 +77,7 @@ int main(void)
         }
     }
 
-    initial = &table[pos[0]-1][pos[1]-1][0][0][0];
+    initial = &table[0][0][pos[0]-1][pos[1]-1][0];
     printf("%lld\n", max(initial->v[0], initial->v[1]));
 
     return 0;
